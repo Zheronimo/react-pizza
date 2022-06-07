42b76dbb0445879e045f19e2b246959b506f68a3
Zheronimo <kirichenko.e.o@gmail.com>
2022-06-07 19:34:02 +0300

    Разработтка поиска и пагинации

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
@@ -11,12 +11,16 @@ import './scss/app.scss';
 const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 
 function App() {
+  const [searchValue, setSearchValue] = useState('');
   return (
     <div className='wrapper'>
-      <Header />
+      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
       <div className='content'>
         <Routes>
-          <Route path='/' element={<Home categoriesList={categories} />} />
+          <Route
+            path='/'
+            element={<Home categoriesList={categories} searchValue={searchValue} />}
+          />
           <Route path='/cart' element={<Cart />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,21 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination';
 
-const Home = ({ categoriesList }) => {
+const Home = ({ categoriesList, searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
   const [categoryId, setCategoryId] = useState(0);
   const [sortType, setSortType] = useState({ name: 'популярности', sortProperty: 'rating' });
 
   useEffect(() => {
     setIsLoading(true);
+    const category = categoryId > 0 ? `&category=${categoryId}` : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
     fetch(
-      `https://6293dd43089f87a57ac6edcf.mockapi.io/item?${
-        categoryId > 0 ? `category=${categoryId}` : ''
-      }&sortBy=${sortType.sortProperty}&order=asc`,
+      `https://6293dd43089f87a57ac6edcf.mockapi.io/item?page=${currentPage}&limit=4${category}&sortBy=${sortType.sortProperty}&order=asc${search}`,
     )
       .then((res) => res.json())
       .then((arr) => {
@@ -24,9 +26,7 @@ const Home = ({ categoriesList }) => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
-
-  console.log(items);
+  }, [categoryId, sortType, searchValue, currentPage]);
 
   return (
     <div className='container'>
@@ -53,6 +53,7 @@ const Home = ({ categoriesList }) => {
               />
             ))}
       </div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 };
